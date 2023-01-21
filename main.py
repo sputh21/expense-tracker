d53d4f04ad3c9b734c598f9e9f4160ec70031cf7
Sahil Puthawala <84691537+sputh21@users.noreply.github.com>
2023-01-21 17:33:46 -0500

    display corrected, toolbar and buttons done

--- main.py
+++ main.py
@@ -1,27 +1,84 @@
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+from PyQt6.QtSql import *
 import sys
-from PyQt6.QtWidgets import QMainWindow, QMenu, QApplication
-from PyQt6.QtGui import QAction
+
 
 import sys
 
+
+class table(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        toolbar = QToolBar("New Bar")
+        toolButton = QToolButton()
+        toolButton.setText("Add Item")
+        toolbar.addWidget(toolButton)
+        self.addToolBar(toolbar)
+        
+
+        
+
+class tabs(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        restaurants = QWidget()
+        restaurants.layout = QHBoxLayout()
+        left = QFrame()
+        left.setFrameShape(QFrame.Shape.StyledPanel)
+        left.layout=QHBoxLayout()
+        newTool = table()
+        left.layout.addWidget(newTool)
+        left.setLayout(left.layout)
+        
+        right = QFrame()
+        right.setFrameShape(QFrame.Shape.StyledPanel)
+        right.layout=QHBoxLayout()
+        right.setLayout(right.layout)
+
+
+        splitter1 = QSplitter(Qt.Orientation.Horizontal)
+        splitter1.addWidget(left)
+        splitter1.addWidget(right)
+       
+        restaurants.setLayout(restaurants.layout)
+        restaurants.layout.addWidget(splitter1)
+
+        streamingServices= QWidget()
+        personalItems = QWidget()
+        groceries = QWidget()
+        bills = QWidget() 
+        summary = QWidget()
+        tabWidget = QTabWidget()
+
+        widgetArr=[restaurants, streamingServices, personalItems, groceries, bills]
+        strArr = ["Restaurants", "Streaming", "Personal Items", "Groceries", "Bills"]
+        #add tabs
+        for i in range(len(strArr)):
+            tabWidget.addTab(widgetArr[i],strArr[i])
+        tabWidget.addTab(summary, "Summary")
+        layout.addWidget(tabWidget, 0, 0)
 
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
-        super().__init__()
-        QWidget.__init__(parent)
+        super().__init__(parent)
         layout = QGridLayout()
         self.setLayout(layout)
-class tabs(QWidget):
-    def __init__(self):
-        layout = QGridLayout()
-        self.setLayout(layout)
+        self.setWindowTitle("Example")
+        tabsWidget = tabs()
+        self.setCentralWidget(tabsWidget)
 
 
-        
+
+
+
+
+
 app = QApplication(sys.argv)
 screen = Window()
 screen.show()
